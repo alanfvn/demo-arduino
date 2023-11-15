d492986f9c597e83b23b6abc2ee5aa8d6a4fbd2e
alanfvn <alandavidg16@gmail.com>
2023-11-15 00:12:00 -0600

    Testing

---
+++ test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Install dependencies
+apt-get update
+apt-get install curl git -y
+
+cd ~
+# Install arduino-cli
+curl -L -o arduino-cli.tar.bz2 https://downloads.arduino.cc/arduino-cli/arduino-cli-latest-linux64.tar.bz2
+tar xjf arduino-cli.tar.bz2
+rm arduino-cli.tar.bz2
+mv `ls -1` /usr/bin/arduino-cli
+
+# Install python, pip and pyserial
+# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+# python get-pip.py
+# pip install pyserial
+
+# Install esp32 core
+printf "board_manager:\n  additional_urls:\n    - https://dl.espressif.com/dl/package_esp32_index.json\n" > .arduino-cli.yaml
+arduino-cli core update-index --config-file .arduino-cli.yaml
+arduino-cli core install esp32:esp32 --config-file .arduino-cli.yaml
+
+# Install 'native' packages
+# arduino-cli lib install "ArduinoJson"
+# cd -
+
+# Install 'third-party' packages: find proper location and 'git clone'
+cd `arduino-cli config dump | grep sketchbook | sed 's/.*\ //'`/libraries
+git clone https://github.com/ayushsharma82/ElegantOTA
+cd -
