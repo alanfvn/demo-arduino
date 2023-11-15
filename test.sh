8f221521a0cce98824ff6fc3308614bba543aeea
alanfvn <alandavidg16@gmail.com>
2023-11-15 00:30:58 -0600

    Updated action

--- test.sh
+++ test.sh
@@ -6,10 +6,10 @@ apt-get install curl git -y
 
 cd ~
 # Install arduino-cli
-# curl -L -o arduino-cli.tar.bz2 https://downloads.arduino.cc/arduino-cli/arduino-cli-latest-linux64.tar.bz2
-# tar xjf arduino-cli.tar.bz2
-# rm arduino-cli.tar.bz2
-# mv `ls -1` /usr/bin/arduino-cli
+curl -L -o arduino-cli.tar.bz2 https://downloads.arduino.cc/arduino-cli/arduino-cli-latest-linux64.tar.bz2
+tar xjf arduino-cli.tar.bz2
+rm arduino-cli.tar.bz2
+mv `ls -1` /usr/bin/arduino-cli
 
 # Install python, pip and pyserial
 # curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
