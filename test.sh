002b857d0d2f9fa15aca6bc32228d8a2ee4bd792
alanfvn <alandavidg16@gmail.com>
2023-11-15 00:50:55 -0600

    Updated

--- test.sh
+++ test.sh
@@ -10,12 +10,7 @@ cd .
 curl -L -o arduino-cli.tar.bz2 https://downloads.arduino.cc/arduino-cli/arduino-cli-latest-linux64.tar.bz2
 tar xjf arduino-cli.tar.bz2
 rm arduino-cli.tar.bz2
-mv arduino-cli /usr/bin/arduino-cli
-
-# Install python, pip and pyserial
-# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-# python get-pip.py
-# pip install pyserial
+sudo mv arduino-cli /usr/bin/arduino-cli
 
 # Install esp32 core
 printf "board_manager:\n  additional_urls:\n    - https://dl.espressif.com/dl/package_esp32_index.json\n" > .arduino-cli.yaml
