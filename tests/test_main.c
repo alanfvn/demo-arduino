909f870aa3acfed7fde23535e59d0020150709cc
alanfvn <alandavidg16@gmail.com>
2023-11-17 14:25:12 -0600

    Updated test to pass again!

--- tests/test_main.c
+++ tests/test_main.c
@@ -2,36 +2,36 @@
 #include "planck_unit.h"
 
 int add_stuff1( int a, int b) {
-	return a + b;
+  return a + b;
 }
 
 void test_addstuff1_2( planck_unit_test_t *tc) {
-	int a	= 10;
-	int b	= 3;
+  int a	= 10;
+  int b	= 3;
 
-	PLANCK_UNIT_ASSERT_TRUE(tc, a + b == add_stuff1(a, b));
+  PLANCK_UNIT_ASSERT_TRUE(tc, a + b == add_stuff1(a, b));
 }
 
 void check_strings( planck_unit_test_t *tc) {
-	char	*a	= "apple";
-	char	*b	= "orange";
+  char	*a	= "apple";
+  char	*b	= "apple";
 
-	PLANCK_UNIT_ASSERT_STR_ARE_EQUAL(tc, a, b);
+  PLANCK_UNIT_ASSERT_STR_ARE_EQUAL(tc, a, b);
 }
 
 
 int main(void) {
-	planck_unit_suite_t *suite;
+  planck_unit_suite_t *suite;
 
-	suite = planck_unit_new_suite();
+  suite = planck_unit_new_suite();
 
   //TESTS
-	PLANCK_UNIT_ADD_TO_SUITE(suite, test_addstuff1_2);
-	PLANCK_UNIT_ADD_TO_SUITE(suite, check_strings);
+  PLANCK_UNIT_ADD_TO_SUITE(suite, test_addstuff1_2);
+  PLANCK_UNIT_ADD_TO_SUITE(suite, check_strings);
 
   //EXECUTE
-	planck_unit_run_suite(suite);
-	planck_unit_destroy_suite(suite);
-	return 0;
+  planck_unit_run_suite(suite);
+  planck_unit_destroy_suite(suite);
+  return 0;
 }
 
